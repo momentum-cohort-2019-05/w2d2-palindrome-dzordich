0aa2cb54c0a0e779687e3f1f8471a3f1c466604a
dzordich <dzordich@gmail.com>
2019-06-04 14:45:56 -0400

    updated with a seperate file that uses recursion to perform same function

---
+++ recur.py
@@ -0,0 +1,21 @@
+words = input("Please enter a string to test if it is a palidrome ")
+letters = "abcdefghijklmnopqrstuvwxyz"
+words = words.lower()
+forward = []
+backward = []
+count = 0
+
+def addLetters(place):
+    if place < len(words):
+        if words[place] in letters:
+            forward.append(words[place])
+            backward.insert(0, words[place])
+            place += 1
+            addLetters(place)
+
+addLetters(count)
+
+if forward == backward:
+    print(words + " is a palidrome!")
+else:
+    print(words + " is not a palidrome.")
